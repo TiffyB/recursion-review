bf52a45087a13ec64371a53682d508ccc0f44e7f
Hack Reactor Student <student@hackreactor.com>
2017-08-21 15:40:01 -0700

    Partial skeleton written for ParseJSON

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -4,4 +4,38 @@
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
   // your code goes here
+  
+
+
+
+  //for example, may receive "[]", which is an array
+  //need to check if first letter is === "["
+    //if yes, create an elements array
+    //remove last bracket
+    //if length of string is greater than 2
+      //check if "," characters are included
+        //if yes, then split by a comma
+        //iterate over split elements and push to elements array
+
+
+  //how to handle primatives
+    //how do we handle strings??? 
+    //note: "\"blah\""
+    //if \" is found
+      //(json is then a string)
+      //remove first \"
+      //find the end pair \" and remove
+      //return resulting string
+
+  //how to handle numbers
+    //check if first character is a number by....isNaN(Number(str)) <- will equal true for non numbers OR check if "-" is the first char
+      //turn string into a number using Number();
+
+  //how to handle booleans
+    //if string is equal to "true" or "false"
+      // if string === "true"
+        //return true
+      //else return false;
+
+  //
 };
